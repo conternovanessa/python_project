45ed82d3c279d7844bc25620d23cb8c97c90b686
Vanessa Conterno <vanessa.conternoedu.unito.it>
2025-03-26 15:25:52 +0100

    project of codedex to create a gif

---
+++ final_project/gif_project/gif.py
@@ -0,0 +1,7 @@
+import imageio.v3 as iio
+filenames = ['frame-01.png','frame-02.png','frame-03.png','frame-04.png','frame-05.png','frame-06.png']
+images =[]
+for filename in filenames:
+    images.append(iio.imread(filename))
+
+iio.imwrite('totaro.gif', images, duration = 500, loop = 0)
